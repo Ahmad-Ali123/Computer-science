597be65f141226b343eddfff0db30e106ce2b1a1
Ahmad-Ali123 <ahmadali7901634@gmail.com>
2022-05-27 09:55:04 -0400

    finished challenge 6 and committing whatever i did to mathquiz.py

---
+++ GASP/GASPchall/GASP_6.py
@@ -0,0 +1,10 @@
+num = 100
+
+question = "give me a number please "
+user_answer = int(input(question))
+
+if user_answer == num:
+  print("whoo your number is equal 100 YAY!")
+else:
+  print("darn your number is not equal to 100 -_-")
+
