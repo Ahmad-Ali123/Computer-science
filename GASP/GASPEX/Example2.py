19a9037f4501e2b4dfcf302fc5c6d3451fd30795
Ahmad-Ali123 <ahmadali7901634@gmail.com>
2022-05-27 10:50:19 -0400

    added new example sheet and fixed some stuff on challenege 9

---
+++ GASP/GASPEX/Example2.py
@@ -0,0 +1,5 @@
+
+odd = 1
+for x in 1, 2, 3, 4, 5, 6:
+  print('An odd number: ', odd)
+  odd = odd + 2
--- GASP/GASPchall/math_quiz.py
+++ GASP/GASPchall/math_quiz.py
@@ -1,14 +1,17 @@
-from random import randint
 
-num1 = randint(1,10)
-num2 = randint(1,10)
+for x in range(10) :
 
-question = "What is " + str(num1) + " times " + str(num2) +"? "
-user_answer = int(input(question))
-correct_answer = num1 * num2
+  from random import randint
+  
+  num1 = randint(1,10)
+  num2 = randint(1,10)
 
-if user_answer == correct_answer:
-  print("thats right - well done.")
-else:
-  print("No, I'm afraid that answer is " + str(correct_answer) + ".")
+  question = "What is " + str(num1) + " times " + str(num2) +"? "
+  user_answer = int(input(question))
+  correct_answer = num1 * num2
+
+  if user_answer == correct_answer:
+    print("thats right - well done.")
+  else:
+    print("No, I'm afraid that answer is " + str(correct_answer) + ".")
 
