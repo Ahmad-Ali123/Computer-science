f5b4bb0f10d53970912b317562790eedcdbc854e
Ahmad-Ali123 <ahmadali7901634@gmail.com>
2022-05-27 10:13:02 -0400

    added a GASP example dir so i can review that code for the future

---
+++ GASP/GASPEX/Example1.py
@@ -0,0 +1,8 @@
+
+for x in 10, 20, 30, 40, 50, 60:
+  n = int(input("Please give me a number: "))
+
+  if n == x:
+    print("You got it! Your number and mine are both " str(x) + ".")
+  else:
+    print("Your number, " + str(n) + " is not " + str(x) + "."
